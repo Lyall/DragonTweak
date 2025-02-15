db317d3e8070134e346476d2b3df0a61d7a16601
Lyall <lyall@stamper.net>
2025-02-15 08:37:28 +0000

    Update.

--- xmake.lua
+++ xmake.lua
@@ -3,12 +3,29 @@ add_rules("mode.debug", "mode.release")
 set_languages("c++latest", "clatest")
 set_optimize("faster")
 
-target("DragonTweak")
-  set_kind("shared")
-  add_files("./src/**.cpp", "./external/safetyhook/safetyhook.cpp", "./external/safetyhook/zydis.c")
-  add_syslinks("user32")
-  add_includedirs("./external/spdlog/include", "./external/inipp", "./external/safetyhook")
-  set_extension(".asi")
+  target("DragonTweak")
+    set_kind("shared")
+    add_files("./src/**.cpp", "./external/safetyhook/safetyhook.cpp", "./external/safetyhook/zydis.c")
+    add_syslinks("user32")
+    add_includedirs("./external/spdlog/include", "./external/inipp", "./external/safetyhook")
+    set_extension(".asi")
 
-  set_toolchains("clang")
-  add_cxflags("-static")
+  -- Set platform specific toolchain
+  if is_plat("windows") then
+    set_toolchains("msvc")
+  elseif is_plat("linux") then 
+    set_toolchains("mingw")
+  end
+
+  -- Toolchain-specific flags
+  local toolchain = get_config("toolchain")
+  if toolchain == "msvc" then
+    add_cxflags("/utf-8")
+    if is_mode("release") then
+      add_cxflags("/MT")
+    elseif is_mode("debug") then
+      add_cxflags("/MTd")
+    end
+  elseif toolchain == "mingw" then
+     add_ldflags("-static", "-static-libgcc", "-static-libstdc++", {force = true})
+  end
