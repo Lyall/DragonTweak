ad7a23c3736ef952fe356f9af2ce98951337a7a7
Lyall <lyall@stamper.net>
2025-02-15 08:58:15 +0000

    Fix prefix.

--- xmake.lua
+++ xmake.lua
@@ -8,6 +8,7 @@ set_optimize("faster")
     add_files("src/**.cpp", "external/safetyhook/safetyhook.cpp", "external/safetyhook/Zydis.c")
     add_syslinks("user32")
     add_includedirs("external/spdlog/include", "external/inipp", "external/safetyhook")
+    set_prefixname("")
     set_extension(".asi")
 
   -- Set platform specific toolchain
