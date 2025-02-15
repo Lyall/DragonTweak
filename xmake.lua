2b89d3eb4fc5ac6e65bfdfabedad8e2160ef5eaa
Lyall <lyall@stamper.net>
2025-02-15 09:50:49 +0000

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -14,19 +14,13 @@ set_optimize("faster")
   -- Set platform specific toolchain
   if is_plat("windows") then
     set_toolchains("msvc")
-  elseif is_plat("linux") then 
-    set_toolchains("mingw")
-  end
-
-  -- Toolchain-specific flags
-  local toolchain = get_config("toolchain")
-  if toolchain == "msvc" then
     add_cxflags("/utf-8")
     if is_mode("release") then
       add_cxflags("/MT")
     elseif is_mode("debug") then
       add_cxflags("/MTd")
     end
-  elseif toolchain == "mingw" then
-    add_ldflags("-static", "-static-libgcc", "-static-libstdc++", "-lstdc++", {force = true})
+  elseif is_plat("linux") then 
+    set_toolchains("mingw")
+    add_ldflags("-static", "-static-libgcc", "-static-libstdc++", {force = true})
   end
